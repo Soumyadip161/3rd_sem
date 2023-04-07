dd8b5fef133432312f4875902ef098d23fa7335e
Soumyadip Ghosh <95485191+Soumyadip161@users.noreply.github.com>
2023-04-07 11:43:31 +0530

    Update mergesort.c

--- mergesort.c
+++ mergesort.c
@@ -1,69 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
-void merge(int *arr,int l,int mid,int h)
+int cost=0;
+void Merge(int *arr,int l,int mid,int h)
 {
-    int i=0,j=0,k=l;
-    int sz1=mid -l+1;
-    int sz2=h-mid;
-    int *A1, *A2;
-    A1=(int *)malloc(sizeof(int)*sz1);
-    A2=(int *)malloc(sizeof(int)*sz2);
+    int l1=mid-l+1;
+    int l2=h-mid;
+    int *arr1=(int *)malloc(sizeof(int)*l1);
+    int *arr2=(int *)malloc(sizeof(int)*l2);
+    
+    int i;
     for(i=l;i<=mid;i++)
     {
-        A1[i-l]=arr[i];
+        arr1[i-l]=arr[i];
+        cost+=3;
     }
-    for(j=mid+1;j<=h;j++)
+    for(i=mid+1;i<=h;i++)
     {
-        A2[j-(mid+1)]=arr[j];
+        arr2[i-(mid+1)]=arr[i];
+        cost+=3;
     }
     i=0;
-    j=0;
-    while(i<sz1 && j<sz2)
+    int j=0;
+    int k=l;
+    while(i<l1 && j<l2)
     {
-        if(A1[i]<A2[j])
+        cost+=2;
+        if(arr1[i]<arr2[j])
         {
-            arr[k]=A1[i];
-            i=i+1;
-            k++;
+            cost+=2;
+            arr[k++]=arr1[i];
+            i++;
         }
         else{
-            arr[k]=A2[j];
+            arr[k++]=arr2[j];
             j++;
-            k++;
+            cost+=2;
         }
     }
-    while(i<sz1)
+    while(i<l1)
     {
-        arr[k]=A1[i];
+        arr[k++]=arr1[i];
         i++;
-        k++;
+        cost+=2;
     }
-     while(j<sz2)
+    while(j<l2)
     {
-        arr[k]=A2[j];
+        arr[k++]=arr2[j];
         j++;
-        k++;
+        cost+=2;
     }
+
 }
-void merge_sort(int *arr,int l,int h)
+void Mergesort(int * arr,int l,int h)
 {
     int mid;
     if(l<h)
     {
-      mid=(l+h)/2;
-      merge_sort(arr,l,mid);
-      merge_sort(arr,mid+1,h);
-      merge(arr,l,mid,h);
+        cost+=2;
+        mid=(l+h)/2;
+        Mergesort(arr,l,mid);
+        Mergesort(arr,mid+1,h);
+        Merge(arr,l,mid,h);
     }
-
 }
 int main()
 {
-    int arr[]={6,4,15,3,37};
-    merge_sort(arr,0,4);
-    for(int i=0;i<=4;i++)
+
+    int *arr;
+    int n,i;
+    printf("Enter the size of the array\n");
+    scanf("%d",&n);
+    arr=(int *)malloc(sizeof(int)*n);
+    printf("Enter the array element\n");
+    
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        
+    }
+    printf("Entered array-------\n");
+    for(int i=0;i<n;i++)
     {
         printf("%d\t",arr[i]);
+        
     }
+    printf("\n");
+    Mergesort(arr,0,n-1);
+     printf("Sorted array-------\n");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d\t",arr[i]);
+        
+    }
+    printf("\n");
+    printf("Total cost=%d\n",cost);
     return 0;
 }
